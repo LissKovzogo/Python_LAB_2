075ef0541de582639f8685ac172fdcfa1cfea81d
LissKovzogo <Silika0Clea@yandex.ru>
2025-09-11 02:22:56 +0300

    Commit python

---
+++ individual.py
@@ -0,0 +1,7 @@
+print("Индивидуальное задание 1, вариант 13")
+age_T = int(input("Введите возраст Тани: "))
+age_M = int(input("Введите возраст Мити: "))
+aver = (age_M + age_T)/2
+print("Средний возраст:", aver)
+print("Митя:", age_M - aver)
+print("Таня:", age_T - aver)
---
+++ numbers.py
@@ -0,0 +1,7 @@
+firsf = int(input("Первое число "))
+second = int(input("Второе число "))
+third = int(input("Третье число "))
+fourth = int(input("Четвёртое число "))
+f = firsf + second
+s = third + fourth
+print(f'{(f/s):.2f}')
